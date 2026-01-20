f6201f779bd3e09d71188b93d6dfa8a1a4d6fc52
dracaenaa <mbk@kolker.org>
2026-01-19 21:15:28 -0500

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
+import GreetingMessage from '@/components/GreetingMessage.vue'
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
+    <GreetingMessage />
     <TheWelcome />
   </main>
 </template>
