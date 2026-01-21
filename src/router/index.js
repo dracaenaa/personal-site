b72a8aa350092072c7ba1a7455f47059223c546b
dracaenaa <mbk@kolker.org>
2026-01-21 11:47:33 -0500

    .

--- src/router/index.js
+++ src/router/index.js
@@ -6,12 +6,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/personal-site/',
+      path: '/',
       name: 'home',
       component: HomeView,
     },
     {
-      path: '/personal-site/about',
+      path: '/about',
       name: 'about',
       component: AboutView,
     },
