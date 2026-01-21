a138ef3db5ccbfef6b50bec91fb35a77bf991ce6
dracaenaa <mbk@kolker.org>
2026-01-21 11:24:41 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'personal-site',
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  base: '/personal-site/',
+  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
