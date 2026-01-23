860b9805437962255a9d0ae9ee89e6f0cbefd89d
dracaenaa <mbk@kolker.org>
2026-01-22 20:56:29 -0500

    test action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,10 +5,10 @@ import { PhGithubLogo, PhInfo } from '@phosphor-icons/vue'
 <template>
   <main>
     <h1 class="pb-5">Welcome!</h1>
-    <h2 class="pb-5">
+    <h3 class="pb-5">
       My name is Miranda. I'm a professional software developer, plus plant parent (say that three
       times fast), pet parent, casual gamer, and newbie snowboarder.
-    </h2>
+    </h3>
     <div class="flex flex-col items-start gap-3 py-5">
       <h2>Check out my Github to see some of my work:</h2>
       <a href="https://github.com/dracaenaa" class="button-style gap-1">
@@ -18,10 +18,10 @@ import { PhGithubLogo, PhInfo } from '@phosphor-icons/vue'
     </div>
     <div class="flex flex-col items-end gap-3">
       <h3>Or learn more about me:</h3>
-      <a href="about" class="button-style gap-1">
+      <RouterLink to="about" class="button-style gap-1">
         <PhInfo size="20px" />
         <h4>about me</h4>
-      </a>
+      </RouterLink>
     </div>
   </main>
 </template>
